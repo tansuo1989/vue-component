3d83e10e6fa4f1c09257278c8ca81c92b04450d0
tansuo19 <huangui19@163.com>
2017-12-09 12:32:44 +0800

    换了数据，去除了不必要的依赖

--- area.vue
+++ area.vue
@@ -3,19 +3,19 @@
 		<div class="row">
 			<div class="col">
 			<select v-model="f.p" @change="selpro">
-				<option :value="i" v-for="(v,i) in pro">{{v.name}}</option>
+				<option :value="i" v-for="(v,i) in pro" :key="v.id">{{v.name}}</option>
 			</select>
 			</div>
 
 			<div class="col">
 			<select v-model="f.c" @change="selcity">
-				<option :value="i" v-for="(v,i) in city">{{v.name}}</option>
+				<option :value="i" v-for="(v,i) in city" :key="v.id">{{v.name}}</option>
 			</select>
 			</div>
 
 			<div class="col">
 			<select v-model="f.cc" v-show="county.length>0" @change="result">
-				<option :value="i" v-for="(v,i) in county">{{v.name}}</option>
+				<option :value="i" v-for="(v,i) in county" :key="v.id">{{v.name}}</option>
 			</select>
 			</div>
 
@@ -62,7 +62,7 @@ export default {
 			this.result();
 		},
 		selcity:function(){
-			this.county=this.city[this.f.c]['child'];
+			this.county=this.city[this.f.c]['child']?this.city[this.f.c]['child']:[];
 			this.f.cc=0;
 			this.result();
 		},
@@ -70,8 +70,12 @@ export default {
 			var re={
 				pro:{id:this.pro[this.f.p].id,name:this.pro[this.f.p].name},
 				city:{id:this.city[this.f.c].id,name:this.city[this.f.c].name},
-				county:{id:this.county[this.f.cc].id,name:this.county[this.f.cc].name}
 			};
+			if(this.county.length>0){
+             re.county={id:this.county[this.f.cc].id,name:this.county[this.f.cc].name};
+			}else{
+				re.county={id:"",name:""};
+			}
 			this.$emit("select",re);
 		}
 	}
